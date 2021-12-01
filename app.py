2d2319c1ecf54d69bc88a2be2d7bdcb868f44e8b
Satpal Singh Rathore <satpal.code@gmail.com>
2021-12-01 18:26:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 from happytransformer import TTSettings
-beam_settings = TTSettings(num_beams=5, min_length=1, max_length=20)
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -16,9 +14,12 @@ def home():
 def predict():
 
     int_features = str(request.form['Input_text'])
+    beam_settings = TTSettings(num_beams=5, min_length=1, max_length=20)
+
+    model = pickle.load(open('model.pkl', 'rb'))
     output = model.generate_text(int_features, args=beam_settings)
 
     return render_template('index.html', prediction_text='{}'.format(output.text))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
